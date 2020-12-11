bac49bc12f09d8156063dc8a02032e94f6565038
Vladislav Valtchev <vladislav.valtchev@gmail.com>
2020-12-11 19:07:24 +0200

    Drop `T get() const` from SharedVal<T>

--- sharedval.h
+++ sharedval.h
@@ -4,8 +4,30 @@
 #include <memory>
 using namespace std;
 
+/*
+ * This special class allows us to store a non-trivial type in a union.
+ * Of course, in order to everything to work, the owner class (EvalValue)
+ * containing an instance of SharedVal<T> inside a union, has to manually
+ * call: the copy ctor, the move ctor, the copy assign and the move assign
+ * methods, specific for this type. This happens in part in EvalValue and
+ * in part in the specific Type instance. See NonTrivialType<T> and SharedType<T>
+ * in evaltypes.cpp.h.
+ *
+ * Q: Why not just using std::variant?
+ * A: Because it's much slower. By just switching to std::variant, even with the
+ *    maximum level of optimization, the hole interpreter gets about 50% slower
+ *    when performing a simple loop. That means that std::variant per se is much
+ *    more slower than +50%. That's unacceptable.
+ */
+
 template <class T>
-struct SharedVal {
+class SharedVal {
+
+    shared_ptr<T> &to_shared_ptr() {
+        return *reinterpret_cast<shared_ptr<T> *>(data);
+    }
+
+public:
 
     typedef T type;
 
@@ -21,17 +43,8 @@ struct SharedVal {
         new ((void *)data) shared_ptr<T>(move(s));
     }
 
-    shared_ptr<T> &to_shared_ptr() {
-        return *reinterpret_cast<shared_ptr<T> *>(data);
-    }
-
     T &get() {
         return *to_shared_ptr().get();
-    }
-
-    T get() const {
-        /* Unfortunately, this const_cast is unavoidable */
-        return *const_cast<SharedVal<T> *>(this).get();
     }
 
     long use_count() {
